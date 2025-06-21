efd031212f091507268948fd5525afdf08e6785f
99x-dgCandidate-Saranga-1737099400352-at-675482622082 <sarangasama@gamail.com>
2025-06-22 04:08:11 +0530

    build responsive topNavigation container

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,18 +1,11 @@
-import RegionLanguageButton from './custom/RegionLanguageButton';
+import BottomNav from "./BottomNav";
+import TopNav from "./TopNav";
 
 function Nav() {
     return(
-        <div className='flex flex-row justify-between p-6'>
-            <div className='flex flex-row w-[220px] justify-between items-center'>
-                <button className='text-sm rounded-sm w-[100px] h-[30px] bg-gray-200'>Hotline 24/7</button>
-                <div className='text-sm font-semibold'>0112 456 789</div>
-            </div>
-            <div className='flex flex-row w-[350px] justify-between items-center'>
-                <div className='text-sm'>Sell on Kade.lk</div>
-                <div className='text-sm'>Order Track</div>
-                <RegionLanguageButton/>
-            </div>
-            
+        <div className="flex flex-col p-6">
+            <TopNav/>
+            <BottomNav/>
         </div>
     );
 }
---
+++ frontend/src/components/nav/custom/Logo.jsx
@@ -0,0 +1,13 @@
+function Logo() {
+    return (
+        <div>
+            <img src="" alt="Logo" />
+            <div>
+                <div>KADE.LK</div>
+                <div>EveryThing</div>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
